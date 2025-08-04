147393e632ff5f1a52077247ff42a4b9225a5d07
sridher <lsridherp@gmail.com>
2025-08-04 11:33:36 +0530

    check data type testing.py

---
+++ Numbers_examples.py
@@ -0,0 +1,18 @@
+A = 5
+B = 8
+c = 9.8
+d = 9.8
+print (int(A))
+print (int(B))
+print (int(c))
+print (float(d))
+x = 6
+z = 5
+print (float(x))
+print (float(z))
+x = "s1"
+y = 3
+z = 3.0
+print (str(x))
+print (str(y))
+print (str(z))
