9b71890b48c141cb88f0ab99ec74d156a7624b3f
sridherlakshmanan <sridherl@jeanmartin.com>
2025-08-18 15:51:10 +0530

    slicing_examples.py

---
+++ slicing_examples.py
@@ -0,0 +1,12 @@
+b = "Hello, World!"
+print(b[2:5])
+b = "Hello, World!"
+print(b[:5])
+b = "Hello, World!"
+print(b[2:])
+b = "Hello, World!"
+print(b[-5:-2])
+s = 'I am Jaspreet'
+print (s[0:8])
+print (s[0:8:2])
+print (s[2:10:2])
